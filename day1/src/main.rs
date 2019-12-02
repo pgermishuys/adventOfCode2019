41da661604354dd93e7a24b9cd2bdf5891768c46
Pieter Germishuys <pieter@pieterg.com>
2019-12-02 06:52:53 +0100

    day1 - part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,17 +6,41 @@ use std::io::BufReader;
 fn calculate_module_fuel(module_weight: i64) -> i64 {
     return round::floor((module_weight / 3) as f64, 2) as i64 - 2;
 }
-fn main() {
-    let file_name = "input.txt";
-    let file = File::open(file_name).unwrap();
-    let reader = BufReader::new(file);
 
-    let mut total = 0;
-    for (_, line) in reader.lines().enumerate() {
+fn calculate_module_fuel_rec(mut total: i64, module_weight: i64) -> i64 {
+    if calculate_module_fuel(module_weight) <= 0 {
+        return total;
+    }
+    let remainder = calculate_module_fuel(module_weight);
+    if remainder <= 0 {
+        return total;
+    }
+    total += remainder;
+    return calculate_module_fuel_rec(total, remainder);
+}
+
+fn main() {
+    let file_name_part1 = "part1.txt";
+    let file_part1 = File::open(file_name_part1).unwrap();
+    let reader_part1 = BufReader::new(file_part1);
+
+    let mut total_part1 = 0;
+    for (_, line) in reader_part1.lines().enumerate() {
         let line = line.unwrap();
         let module_weight = line.parse::<i64>().unwrap();
-        total += calculate_module_fuel(module_weight);
-        println!("{}", module_weight);
+        total_part1 += calculate_module_fuel(module_weight);
     }
-    println!("total: {}", total);
+    println!("Part One: {}", total_part1);
+
+    let file_name_part2 = "part2.txt";
+    let file_part2 = File::open(file_name_part2).unwrap();
+    let reader_part2 = BufReader::new(file_part2);
+
+    let mut total_part2 = 0;
+    for (_, line) in reader_part2.lines().enumerate() {
+        let line = line.unwrap();
+        let module_weight = line.parse::<i64>().unwrap();
+        total_part2 += calculate_module_fuel_rec(0, module_weight);
+    }
+    println!("Part Two: {}", total_part2);
 }
