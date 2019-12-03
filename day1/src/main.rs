b1b40a8e14b8c1fa465dd5ed13a0095362770348
Pieter Germishuys <pieter@pieterg.com>
2019-12-03 11:50:09 +0100

    Use i32

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,11 +3,11 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-fn calculate_module_fuel(module_weight: i64) -> i64 {
-    return round::floor((module_weight / 3) as f64, 2) as i64 - 2;
+fn calculate_module_fuel(module_weight: i32) -> i32 {
+    return round::floor((module_weight / 3).into(), 2) as i32 - 2;
 }
 
-fn calculate_module_fuel_rec(mut total: i64, module_weight: i64) -> i64 {
+fn calculate_module_fuel_rec(mut total: i32, module_weight: i32) -> i32 {
     if calculate_module_fuel(module_weight) <= 0 {
         return total;
     }
@@ -27,7 +27,7 @@ fn main() {
     let mut total_part1 = 0;
     for (_, line) in reader_part1.lines().enumerate() {
         let line = line.unwrap();
-        let module_weight = line.parse::<i64>().unwrap();
+        let module_weight = line.parse::<i32>().unwrap();
         total_part1 += calculate_module_fuel(module_weight);
     }
     println!("Part One: {}", total_part1);
@@ -39,7 +39,7 @@ fn main() {
     let mut total_part2 = 0;
     for (_, line) in reader_part2.lines().enumerate() {
         let line = line.unwrap();
-        let module_weight = line.parse::<i64>().unwrap();
+        let module_weight = line.parse::<i32>().unwrap();
         total_part2 += calculate_module_fuel_rec(0, module_weight);
     }
     println!("Part Two: {}", total_part2);
